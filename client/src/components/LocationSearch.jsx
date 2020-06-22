8337892fee2eeae3945b8174da54e422ea86c36c
Grant Belcher <gtbelch@gmail.com>
2020-06-22 12:36:34 -0700

    completes basic functionality for location search

--- client/src/components/LocationSearch.jsx
+++ client/src/components/LocationSearch.jsx
@@ -18,9 +18,10 @@ const styles = {
 const LocationSearch = () => {
   const [results, setResults] = useState([]);
   const [text, setText] = useState('');
+  const [location, setLocation] = useState(null);
 
   useEffect(() => {
-    axios.get(`https://www.mapquestapi.com/search/v4/place?sort=relevance&feedback=false&key=WJNdcYkfrIa3bppQNxjGstg6ZJl86IaZ&limit=5&q=${text}`)
+    axios.get(`https://www.mapquestapi.com/search/v4/place?sort=relevance&feedback=false&key=WJNdcYkfrIa3bppQNxjGstg6ZJl86IaZ&limit=4&q=${text}`)
       .then((response) => {
         const { data } = response;
         return data.results.map((location) => location.name);
@@ -32,37 +33,57 @@ const LocationSearch = () => {
       });
   }, [text]);
 
+
   const searchLocation = (event) => {
-    console.log(event.target.value, 'yooo');
-    setText(event.target.value);
+    if (!location) {
+      setText(event.target.value);
+    }
   };
 
+  const selectLocation = (e) => {
+    setLocation(e.target.innerText),
+    setText(e.target.innerText);
+    setResults([]);
+  };
+
+  const handleDelete = (e) => {
+    if (e.key === 'Backspace') {
+      setLocation(null);
+    }
+  };
+  
   let listItems;
   if (results) {
-    listItems = results.map((location) => (
-      <ListItem button>
-        <ListItemText primary={`${location}`} />
+    listItems = results.map((location, i) => (
+      <ListItem button onClick={selectLocation}>
+        <ListItemText primary={`${location}`} key={i + 1} />
       </ListItem>
     ));
   }
 
+  console.log((listItems.length > 0 && location !== null), listItems.length > 0, location)
   return (
     <>
       <TextField
         id="Search Location"
         getOptionLabel={(option) => option.title}
         style={styles.input}
-        label="Search Location"
+        label="Add Location"
         value={text}
+        onKeyDown={handleDelete}
         onChange={(e) => searchLocation(e)}
       />
-      {listItems.length && (
+      {(listItems.length > 0) && (
         <Paper style={{
           width: '70vw',
           marginRight: '20vw',
+          display: (location !== null ? 'none' : 'block')
         }}
         >
           <List>
+            <ListItem button onClick={selectLocation}>
+              <ListItemText primary={`${text}`} />
+            </ListItem>
             {listItems}
           </List>
         </Paper>
