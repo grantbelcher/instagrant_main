c52f51bf33b6fc69513daeedac30485c91809ce0
Grant Belcher <gtbelch@gmail.com>
2020-06-23 14:00:54 -0700

    completes styling footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,41 +1,42 @@
 import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
 
 const styles = {
   footer: {
     backgroundColor: '#eff0f1',
-    top: '85vh',
+    top: '90vh',
     position: 'fixed',
     width: '100vw',
     height: '10vh',
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
-    paddingTop: 5,
+    // alignItems: 'center',
+    // justifyContent: 'space-around',
+    // paddingTop: 5,
     borderTop: 'solid',
     borderWidth: 'thin',
     borderColor: '#b3b4b5',
   },
-}
+  icons: {
+    marginTop: '3vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingTop: 5,
+  },
+};
 
 const Footer = () => {
   return (
     <div style={styles.footer}>
-        <div style={{
-          color: '#8e8e8e',
-          marginTop: '1vh',
-          fontSize: 'smaller',
-        }}
-        >
-          from
-        </div>
-        <div style={{
-          marginTop: '1vh',
-          fontSize: 'small',
-        }}
-        >
-          GRANTBOOK
-        </div>
+      <div style={styles.icons}>
+        <i class="fa fa-home fa-lg" aria-hidden="true"/>
+        <i class="fa fa-search fa-lg" aria-hidden="true"/>
+        <i class="fa fa-plus-square fa-lg" aria-hidden="true" />
+        <i class="fa fa-plus-square fa-lg" aria-hidden="true" />
+        <i class="fa fa-plus-square fa-lg" aria-hidden="true" />
       </div>
+    </div>
   )
 }
 
