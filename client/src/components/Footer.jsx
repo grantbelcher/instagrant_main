aae8daa0bb9e2d792ab7488baafbdbeb6e314868
Grant Belcher <gtbelch@gmail.com>
2020-06-23 14:12:14 -0700

    adds heart icon and avatar to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 import Avatar from '@material-ui/core/Avatar';
 
 const styles = {
@@ -24,20 +25,32 @@ const styles = {
     justifyContent: 'space-around',
     paddingTop: 5,
   },
+  avatarIcon: {
+    height: '4vh',
+    width: '4vh',
+  }
 };
 
-const Footer = () => {
+const Footer = ({ avatar, user }) => {
   return (
     <div style={styles.footer}>
       <div style={styles.icons}>
         <i class="fa fa-home fa-lg" aria-hidden="true"/>
         <i class="fa fa-search fa-lg" aria-hidden="true"/>
         <i class="fa fa-plus-square fa-lg" aria-hidden="true" />
-        <i class="fa fa-plus-square fa-lg" aria-hidden="true" />
-        <i class="fa fa-plus-square fa-lg" aria-hidden="true" />
+        <i class="fa fa-heart-o fa-lg" aria-hidden="true" />
+        <Avatar src={avatar} alt={user} style={styles.avatarIcon} />
       </div>
     </div>
   )
 }
 
-export default Footer;
+const mapStateToProps = ({ auth }) => {
+  const { user, userId, avatar } = auth;
+  return {
+    avatar,
+    user,
+  };
+};
+
+export default connect(mapStateToProps, null)(Footer);
