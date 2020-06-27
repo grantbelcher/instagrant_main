f95c1496ffce36f422f3d7010ca706ef02c34236
Grant Belcher <gtbelch@gmail.com>
2020-06-27 08:50:37 -0700

    styles FollowButton based on profile info

--- client/src/components/FollowButton.jsx
+++ client/src/components/FollowButton.jsx
@@ -21,29 +21,71 @@ const StyledButton = withStyles({
   },
 })(Button);
 
+const AlternateButton = withStyles({
+  root: {
+    background: 'linear-gradient(45deg, #c442bb 30%, #f77ea4 70%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    paddingLeft: '20vw',
+    paddingRight: '22vw',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+})(Button);
 
-const FollowButton = ({ userId, profileId, followUser }) => {
-  const buttonText = 'Follow';
+
+const FollowButton = ({
+  userId, profileId, followUser, following, myFollowers,
+}) => {
+  const alreadyFollowing = following.includes(profileId);
+  const isFollowingMe = myFollowers.includes(profileId);
+  const myProfile = (userId === profileId)
+  let buttonText;
+  if (isFollowingMe && !alreadyFollowing) {
+    buttonText = 'Follow Back';
+  } else if (alreadyFollowing) {
+    buttonText = 'Unfollow';
+  } else if (myProfile) {
+    buttonText = 'Edit Profile';
+  } else {
+    buttonText = 'Follow';
+  }
   const toggleFollow = () => {
     followUser(userId, profileId);
   };
 
+  if (alreadyFollowing || myProfile) {
+    return (
+      <AlternateButton
+        onClick={alreadyFollowing ? null : toggleFollow}
+      >
+        {buttonText}
+      </AlternateButton>
+    );
+  }
   return (
     <StyledButton
-      onClick={toggleFollow}
+      onClick={alreadyFollowing ? null : toggleFollow}
     >
       {buttonText}
     </StyledButton>
   );
 };
 
-const mapStateToProps = ({ auth, view }) => {
+const mapStateToProps = ({ auth, view, followStats }) => {
   const { userId } = auth;
   const { profileInfo } = view;
   const { userId: profileId } = profileInfo;
+  const { following, myFollowers } = followStats;
+
   return {
     userId,
     profileId,
+    following,
+    myFollowers,
   };
 };
 
